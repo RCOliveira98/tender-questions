34753d18f8e7508f678b231a93b5514a5bfafaf5
RCOliveira98 <rcoliveira.ti@gmail.com>
2021-01-10 19:16:35 -0300

    permitindo atualizaçao de administradores.

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -4,7 +4,28 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   end
 
   def edit
-    @admin = Admin.find(params[:id])
+    @admin = find_param_by_id()
   end
+
+  def update
+    @admin = find_param_by_id()
+
+    if @admin.update(strong_paramenters())
+      redirect_to admins_backoffice_admins_path, notice: "Admnistrador #{@admin.id} atualizado!"
+    else
+      render :edit
+    end
+
+  end
+
+  private
+  def find_param_by_id
+    Admin.find(params[:id])
+  end
+
+  def strong_paramenters
+    params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
+  
 
 end
