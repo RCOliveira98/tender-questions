93dd0fc473a522254131053db89e15434763010e
RCOliveira98 <rcoliveira.ti@gmail.com>
2021-01-13 16:53:10 -0300

    Criar ação e view de atualizar pergunta

--- app/controllers/admins_backoffice/questions_controller.rb
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -1,7 +1,37 @@
 class AdminsBackoffice::QuestionsController < AdminsBackofficeController
+    before_action :find_question_by_id, only: [:edit, :update]
+    before_action :select_subjects, only: [:edit]
 
     def index
         @questions = Question.all().page(params[:page])
     end
+
+    def edit
+    end
+
+    def update
+        if @question.update(question_params())
+            redirect_to admins_backoffice_questions_path, notice: "Pergunta de ID #{@question.id} atualizada com sucesso!"
+        else
+            render :edit
+        end
+    end
+    
+    private
+
+    def find_question_by_id
+        @question = Question.find(params[:id])
+    end
+
+    def question_params
+        params.require(:question).permit(:description, :subject_id)
+    end
+
+    def select_subjects
+        @subjects = Subject.all().order(:description)
+    end
+    
+    
+    
     
 end
