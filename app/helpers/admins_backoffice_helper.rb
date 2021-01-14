f162ba32c20ed8dd3691fd5735c9ad871900cfdb
RCOliveira98 <rcoliveira.ti@gmail.com>
2021-01-14 09:52:09 -0300

    Atualizar arquivo de tradução models pt-br e index das views

--- app/helpers/admins_backoffice_helper.rb
+++ app/helpers/admins_backoffice_helper.rb
@@ -1,2 +1,5 @@
 module AdminsBackofficeHelper
+    def t_attributes(object = nil, method = nil)
+        object && method ? object.model.human_attribute_name(method) : 'Parâmetros não enviados para tradução'
+    end
 end
