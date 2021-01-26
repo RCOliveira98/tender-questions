bd813e0939496923bd3ac6690290332209bc82c1
RCOliveira98 <rcoliveira.ti@gmail.com>
2021-01-26 16:16:32 -0300

    Adicionar suporte ao active storage

--- app/controllers/users_backoffice/profile_controller.rb
+++ app/controllers/users_backoffice/profile_controller.rb
@@ -22,7 +22,7 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
 
   def params_user
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation,
-      user_profile_attributes: [:id, :gender, :address, :birthdate])
+      user_profile_attributes: [:id, :gender, :address, :birthdate, :avatar])
   end
 
   def verify_password
--- app/controllers/users_backoffice_controller.rb
+++ app/controllers/users_backoffice_controller.rb
@@ -1,4 +1,10 @@
 class UsersBackofficeController < ApplicationController
     before_action :authenticate_user!
+    before_action :build_user_profile
     layout 'users_backoffice'
+
+    private
+    def build_user_profile
+        current_user.build_user_profile if current_user.user_profile.blank?
+    end
 end
