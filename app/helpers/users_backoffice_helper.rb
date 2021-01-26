5cc51ed6c4189a0d964c8a680dd5d776dcda738b
RCOliveira98 <rcoliveira.ti@gmail.com>
2021-01-26 16:16:02 -0300

    Adicionar helpers para nome do usuário logado e url_avatar

--- app/helpers/users_backoffice_helper.rb
+++ app/helpers/users_backoffice_helper.rb
@@ -1,2 +1,10 @@
 module UsersBackofficeHelper
+    def url_avatar(user)
+        avatar = user.user_profile.avatar
+        avatar.attached? ? avatar : 'avatar-admin.png'
+    end
+
+    def name_user(user)
+        user.first_name ? user.full_name : user.email 
+    end
 end
